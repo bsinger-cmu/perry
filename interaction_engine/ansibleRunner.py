36cf809cfcda411dd569f772478066f7daa025f9
Brian <briansin@andrew.cmu.edu>
2022-07-11 13:19:44 -0400

    non-jank ssh key

--- interaction_engine/ansibleRunner.py
+++ interaction_engine/ansibleRunner.py
@@ -1,14 +1,21 @@
 import ansible_runner
+import argparse
 import openstackAPI
 
-def main():
+
+def main(args):
     conn = openstackAPI.initialize()
     manage_server, manage_ip = openstackAPI.find_manage_server(conn)
-    ansible_vars = {'manage_ip': manage_ip}
+    ansible_vars = {'manage_ip': manage_ip, 'ssh_key_path': args.ssh_key_path}
 
     r = ansible_runner.run(extravars=ansible_vars, private_data_dir='../ansible/cage/', playbook='testPlaybook.yml')
 
     print("{}: {}".format(r.status, r.rc))
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ssh_key_path', help='The path to your openstack ssh key')
+    args = parser.parse_args()
+
+    main(args)
