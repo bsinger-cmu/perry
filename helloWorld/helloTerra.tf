76bc5209f186ee042de412c4d6c4ab924642c5d3
aduyinuo <yinuod@andrew.cmu.edu>
2022-07-19 08:28:12 -0400

    commit before switch

--- helloWorld/helloTerra.tf
+++ helloWorld/helloTerra.tf
@@ -3,21 +3,27 @@ required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.35.0"
+      version = "1.47.0"
     }
   }
 }
 
 # Configure the OpenStack Provider
 provider "openstack" {
-  user_name   = "admin"
-  tenant_name = "admin"
-  password    = "pwd"
-  auth_url    = "http://myauthurl:5000/v2.0"
-  region      = "RegionOne"
+  # user_name   = "yinuo"
+  # tenant_name = "admin"
+  # password    = "dvz0ght9CQY*jpd7eng"
+  auth_url    = "https://128.237.154.129:5000/v3/"
+  region      = "microstack"
+  insecure    = "true"
 }
 
 # Create a web server
-resource "openstack_compute_instance_v2" "test-server" {
-  # ...
+resource "openstack_compute_instance_v2" "basic" {
+  name = "basic"
+  flavor_name     = "m1.small"
+  security_groups = ["default"]
+  network {
+    name = "int-net"
+  }
 }
