45bd2c56a9ef6f90115831d2738608803d39452f
Brian <briansin@andrew.cmu.edu>
2022-08-01 12:04:37 -0400

    Add generic command

--- interaction_engine/ansibleRunner.py
+++ interaction_engine/ansibleRunner.py
@@ -1,16 +1,38 @@
 import ansible_runner
 import argparse
 import openstackAPI
+from rich import print
 
+
+def run_bash_command(ansible_def_vars, data_dir, command):
+    ansible_def_vars['command'] = command
+    r = ansible_runner.run(extravars=ansible_def_vars, private_data_dir=data_dir, playbook='generic_commands.yml')
+
+    output = []
+
+    for event in r.events:
+        if 'event_data' in event:
+            if 'res' in event['event_data']:
+                if 'cmd' in event['event_data']['res'] and len(event['event_data']['res']['cmd']) > 0:
+                    if event['event_data']['res']['cmd'][0] == 'pwd':
+                        output.append(event['event_data']['res']['stdout_lines'])
+    
+    return output
 
 def main(args):
     conn = openstackAPI.initialize()
     manage_server, manage_ip = openstackAPI.find_manage_server(conn)
-    ansible_vars = {'manage_ip': manage_ip, 'ssh_key_path': args.ssh_key_path}
+    ansible_data_dir = '../ansible/cage/'
+    ansible_vars_default = {'manage_ip': manage_ip, 'ssh_key_path': args.ssh_key_path}
 
-    r = ansible_runner.run(extravars=ansible_vars, private_data_dir='../ansible/cage/', playbook='testPlaybook.yml')
+    # r = ansible_runner.run(extravars=ansible_vars, private_data_dir='../ansible/cage/', playbook='testPlaybook.yml')
+    # r = ansible_runner.run(extravars=ansible_vars, private_data_dir='../ansible/cage/', playbook='generic_commands.yml')
 
-    print("{}: {}".format(r.status, r.rc))
+    # print("{}: {}".format(r.status, r.rc))
+    # data = r.stdout.read()
+    output = run_bash_command(ansible_vars_default, ansible_data_dir, 'pwd')
+    print(output)
+
 
 
 if __name__ == "__main__":
