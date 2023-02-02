833fc3f99be25c6663f6cf9e2fd3864252619d93
Brian <briansin@andrew.cmu.edu>
2023-02-02 15:57:55 -0500

    Add multithreaded telemetry server

--- interaction_engine/defender/TelemetryServer.py
+++ interaction_engine/defender/TelemetryServer.py
@@ -1,21 +1,42 @@
 # Simple server for receiving telemetry data
-from flask import Flask
-from flask import request
+from flask import Flask, Response, request
+import threading
 
-app = Flask(__name__)
-port = 5000
+host_name = "localhost"
+port = 8001
 
-@app.route("/alert", methods = ['POST', 'GET'])
+
+class EndpointAction(object):
+
+    def __init__(self, action):
+        self.action = action
+        self.response = Response(status=200, headers={})
+
+    def __call__(self, *args):
+        self.action()
+        return self.response
+
+
+class FlaskAppWrapper(object):
+    app = None
+
+    def __init__(self, name):
+        self.app = Flask(name)
+
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=None):
+        self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler), methods=methods)
+
 def handle_alert():
     data = request.form
     honey_type = data['type']
     service_type = data['protocol']
 
     # ToDo send this to defender
-
     return "Received!"
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=port)
+    server = FlaskAppWrapper('TelemetryServer')
+    server.add_endpoint(endpoint='/alert', endpoint_name='ad', handler=handle_alert, methods=['POST', 'GET'])
 
+    threading.Thread(target=lambda: server.app.run(host=host_name, port=port, debug=True, use_reloader=False)).start()
