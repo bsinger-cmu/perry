4be29637cc58193efdec31bb53b7c0f1bceae61a
Yusuf Saquib <yusufsaquib@gmail.com>
2023-07-10 16:42:06 -0400

    update reset script to be cleaner

--- reset_openstack.py
+++ reset_openstack.py
@@ -6,11 +6,15 @@ import sys
 class OpenstackResetter():
     def __init__(self, conn) -> None:
         self.conn = conn
+        self.current_index = 0
 
-    def print_message(self, message, index = 1, total = 1):
-        print("[RESET]\t(%d/%d)\t%s..." % (index, total, message))
+    def print_message(self, message,index = 1, total = 1, inc_cur = True):
+        if (inc_cur):
+            self.current_index += 1
+        print(f"[RESET ({self.current_index}/{self.total_items})]\t({index}/{total})\t{message}...")
 
     def list_all(self):
+        self.initialize_lists()
         self.list_instances()
         self.list_floating_ips()
         self.list_routers()
@@ -20,7 +24,8 @@ class OpenstackResetter():
         self.list_sec_groups()
 
     def reset(self):
-        self.print_message("Resetting Openstack")
+        print(">---       Resetting Openstack       ---<")
+        self.initialize_lists()
         self.delete_instances()
         self.release_floating_ips()
         self.delete_routers()
@@ -28,20 +33,33 @@ class OpenstackResetter():
         self.delete_subnets()
         self.delete_networks()
         self.delete_sec_groups()
-        self.print_message("Finished!")
+        print(">---            Finished!            ---<")
+
+    def initialize_lists(self):
+        self.all_servers        = self.conn.list_servers()
+        self.all_floating_ips   = self.conn.list_floating_ips()
+        self.all_routers        = self.conn.list_routers()
+        self.all_ports          = self.conn.list_ports()
+        self.all_subnets        = self.conn.list_subnets()
+        self.all_networks       = self.conn.list_networks()
+        self.all_sec_groups     = self.conn.list_security_groups()
+        self.total_instances    = len(self.all_servers)
+        self.total_floating_ips = len(self.all_floating_ips)
+        self.total_routers      = len(self.all_routers)
+        self.total_ports        = len(self.all_ports)
+        self.total_subnets      = len(self.all_subnets)
+        self.total_networks     = len(self.all_networks)
+        self.total_sec_groups   = len(self.all_sec_groups)
+        self.total_items = self.total_instances + self.total_floating_ips + self.total_routers + self.total_ports + self.total_subnets + self.total_networks + self.total_sec_groups
 
     def list_instances(self):
-        all_servers = self.conn.list_servers()
-        print("")
-        print("Instances:")
-        for server in all_servers:
+        print("\nInstances:")
+        for server in self.all_servers:
             print("[%s] - %s" % (server.id, server.name))
 
     def list_floating_ips(self):
-        all_floating_ips = self.conn.list_floating_ips()
-        print("")
-        print("Floating IPs:")
-        for ip in all_floating_ips:
+        print("\nFloating IPs:")
+        for ip in self.all_floating_ips:
             print("[%s] - %s (fixed: %s)" % (ip.id, ip.floating_ip_address, ip.fixed_ip_address))
     
     def list_interfaces(self, router):
@@ -53,57 +71,45 @@ class OpenstackResetter():
                 print("    ~ [%s] - Subnet Ip Address: %s" % (fixed_ip['subnet_id'], fixed_ip['ip_address']))
 
     def list_routers(self):
-        all_routers = self.conn.list_routers()
-        print("")
-        print("Routers:")
-        for router in all_routers:
+        print("\nRouters:")
+        for router in self.all_routers:
             print("[%s] - %s" % (router.id, router.name))
             self.list_interfaces(router)
 
     def list_network_ports(self):
-        all_ports = self.conn.list_ports()
-        print("")
-        print("Ports:")
-        for port in all_ports:
+        print("\nPorts:")
+        for port in self.all_ports:
             print("[%s] - %s" % (port.id, port.name))
 
     def list_subnets(self):
-        all_subnets = self.conn.list_subnets()
-        print("")
-        print("Subnets:")
-        for subnet in all_subnets:
+        print("\nSubnets:")
+        for subnet in self.all_subnets:
             print("[%s] - %s" % (subnet.id, subnet.name))
 
     def list_networks(self):
-        all_networks = self.conn.list_networks()
-        print("")
-        print("Networks:")
-        for network in all_networks:
+        print("\nNetworks:")
+        for network in self.all_networks:
             print("[%s] - %s" % (network.id, network.name))
 
     def list_sec_groups(self):
-        all_sec_groups = self.conn.list_security_groups()
-        print("")
-        print("Security Groups:")
-        for sec_group in all_sec_groups:
+        print("\nSecurity Groups:")
+        for sec_group in self.all_sec_groups:
             print("[%s] - %s" % (sec_group.id, sec_group.name))
 
 
     def delete_instances(self):
-        all_servers = self.conn.list_servers()
         current_server_index = 0
-        for server in all_servers:
+        for server in self.all_servers:
             current_server_index += 1
-            self.print_message("Deleting instance %s" % server.name, current_server_index, len(all_servers))
+            self.print_message("Deleting instance %s" % server.name, current_server_index, self.total_instances)
             self.conn.delete_server(server.id)
 
     def release_floating_ips(self):
-        all_floating_ips = self.conn.list_floating_ips()
         current_ip_index = 0
-        for ip in all_floating_ips:
+        for ip in self.all_floating_ips:
             current_ip_index += 1 
             message = "Releasing floating IP (floating: %s, fixed: %s)..." % (ip.floating_ip_address, ip.fixed_ip_address)
-            self.print_message(message, current_ip_index, len(all_floating_ips))
+            self.print_message(message, current_ip_index, self.total_floating_ips)
             self.conn.delete_floating_ip(ip.id)
 
     def delete_interfaces(self, router):
@@ -111,55 +117,50 @@ class OpenstackResetter():
         current_interface_index = 0
         for interface in all_interfaces:
             current_interface_index += 1
-            self.print_message("Deleting interface %s" % interface.id, current_interface_index, len(all_interfaces))
+            self.print_message("Deleting interface %s" % interface.id, current_interface_index, len(all_interfaces), False)
             for fixed_ip in interface.fixed_ips:
                 self.conn.remove_router_interface(router, subnet_id=fixed_ip['subnet_id'])
 
     def delete_routers(self):
-        all_routers = self.conn.list_routers()
         current_router_index = 0
-        for router in all_routers:
+        for router in self.all_routers:
             current_router_index += 1
-            self.print_message("Deleting router %s" % router.name, current_router_index, len(all_routers))
+            self.print_message("Deleting router %s" % router.name, current_router_index, self.total_routers)
             self.delete_interfaces(router)
             self.conn.delete_router(router.id)
 
     def delete_network_ports(self):
-        all_ports = self.conn.list_ports()
         current_port_index = 0
-        for port in all_ports:
+        for port in self.all_ports:
             current_port_index += 1
-            self.print_message("Deleting port %s" % port.name, current_port_index, len(all_ports))
+            self.print_message("Deleting port %s" % port.name, current_port_index, self.total_ports)
             self.conn.delete_port(port.id)
     
     def delete_subnets(self):
-        all_subnets = self.conn.list_subnets()
         current_subnet_index = 0
-        for subnet in all_subnets:
+        for subnet in self.all_subnets:
             current_subnet_index += 1
-            self.print_message("Deleting subnet %s" % subnet.name, current_subnet_index, len(all_subnets))
+            self.print_message("Deleting subnet %s" % subnet.name, current_subnet_index, self.total_subnets)
             self.conn.delete_subnet(subnet.id)
 
     def delete_networks(self):
-        all_networks = self.conn.list_networks()
         current_network_index = 0
-        for network in all_networks:
+        for network in self.all_networks:
             current_network_index += 1
             if network.name == "external":
-                self.print_message("Forbidden to delete network %s. Skipping" % network.name, current_network_index, len(all_networks))
+                self.print_message("Forbidden to delete network %s. Skipping" % network.name, current_network_index, self.total_networks)
                 continue
-            self.print_message("Deleting network %s" % network.name, current_network_index, len(all_networks))
+            self.print_message("Deleting network %s" % network.name, current_network_index, self.total_networks)
             self.conn.delete_network(network.id)
     
     def delete_sec_groups(self):
-        all_sec_groups = self.conn.list_security_groups()
         current_sec_group_index = 0
-        for sec_group in all_sec_groups:
+        for sec_group in self.all_sec_groups:
             current_sec_group_index += 1
             if sec_group.name == "default":
-                self.print_message("Forbidden to delete security group %s. Skipping" % sec_group.name, current_sec_group_index, len(all_sec_groups))
+                self.print_message("Forbidden to delete security group %s. Skipping" % sec_group.name, current_sec_group_index, self.total_sec_groups)
                 continue
-            self.print_message("Deleting security group %s" % sec_group.name, current_sec_group_index, len(all_sec_groups))
+            self.print_message("Deleting security group %s" % sec_group.name, current_sec_group_index, self.total_sec_groups)
             self.conn.delete_security_group(sec_group.id)
 
 
