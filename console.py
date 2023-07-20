f63e59e46a2987e22c4f3deb36976ec69fd04676
Yusuf Saquib <yusufsaquib@gmail.com>
2023-07-20 16:32:33 -0400

    add console module for nice printing

---
+++ console.py
@@ -0,0 +1,27 @@
+from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    Progress,
+    TaskID,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+    TaskProgressColumn,
+    MofNCompleteColumn,
+    TransferSpeedColumn,
+    SpinnerColumn,
+)
+console = Console()
+progress = Progress(
+    SpinnerColumn(),
+    TextColumn("[progress.description]{task.description}"),
+    BarColumn(),
+    TaskProgressColumn(),
+    MofNCompleteColumn(),
+    TimeRemainingColumn(), 
+    TimeElapsedColumn(),
+    console=console,
+    refresh_per_second=10,
+    # transient=True,
+)
