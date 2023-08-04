a374ff5d8356e6fa8eddd6db57174e879fe8a550
Yusuf Saquib <yusufsaquib@gmail.com>
2023-08-04 14:20:33 -0400

    update honeyservice

--- defender/capabilities/StartHoneyService.py
+++ defender/capabilities/StartHoneyService.py
@@ -4,7 +4,9 @@ class StartHoneyService(Action):
 
     name = 'start_honey_service'
     
-    def __init__(self, host):
+    def __init__(self, host, port_no='8000', service='ssh'):
         super().__init__()
 
         self.host = host
+        self.port_no = port_no
+        self.service = service
--- defender/orchestrator/openstack_actuators/StartHoneyService.py
+++ defender/orchestrator/openstack_actuators/StartHoneyService.py
@@ -4,6 +4,7 @@ from .OpenstackActuator import OpenstackActuator
 class StartHoneyService(OpenstackActuator):
 
     def actuate(self, action):
+        # params = {'host': action.host, 'port_no': action.port_no, 'service': action.service, 'elasticsearch_server': self.external_elasticsearch_server, 'elasticsearch_api_key': self.elasticsearch_api_key}
         params = {'host': action.host, 'elasticsearch_server': self.external_elasticsearch_server, 'elasticsearch_api_key': self.elasticsearch_api_key}
-        print(params)
+        # self.ansible_runner.run_playbook('defender/deploy_honey_service.yml', playbook_params=params)
         self.ansible_runner.run_playbook('defender/deploy_honey_ssh.yml', playbook_params=params)
