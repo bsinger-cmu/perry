86619712abc61e1e2ba82351096bf0d37e2b5bbc
Brian <briansin@andrew.cmu.edu>
2024-05-02 15:33:44 -0400

    Add large equifax deployment instance

--- deployment_instance/EquifaxLarge.py
+++ deployment_instance/EquifaxLarge.py
@@ -2,12 +2,12 @@ from .EquifaxInstance import EquifaxInstance
 from ansible.AnsibleRunner import AnsibleRunner
 
 
-class EquifaxMedium(EquifaxInstance):
+class EquifaxLarge(EquifaxInstance):
     def __init__(
         self,
         ansible_runner: AnsibleRunner,
         openstack_conn,
         caldera_ip,
     ):
-        topology = "equifax_medium"
+        topology = "equifax_large"
         super().__init__(ansible_runner, openstack_conn, caldera_ip, topology)
--- deployment_instance/__init__.py
+++ deployment_instance/__init__.py
@@ -3,6 +3,7 @@ from .SimpleInstanceV1 import SimpleInstanceV1
 from .TwoPathInstance import TwoPathInstance
 from .EnterpriseInstance import EnterpriseInstance
 from .EquifaxInstance import EquifaxInstance
+from .EquifaxLarge import EquifaxLarge
 from .EquifaxMedium import EquifaxMedium
 from .EquifaxSmall import EquifaxSmall
 from .SolarWindsInstance import SolarWindsInstance
