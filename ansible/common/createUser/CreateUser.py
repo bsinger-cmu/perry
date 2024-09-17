574f4c893b476a33d61069a0c146f3cfb76ad73d
Brian <brian@convolve.com>
2024-09-16 20:51:10 -0400

    Add local ansible runners for docker

--- ansible/common/createUser/CreateUser.py
+++ ansible/common/createUser/CreateUser.py
@@ -2,6 +2,6 @@ from ansible.AnsiblePlaybook import AnsiblePlaybook
 
 
 class CreateUser(AnsiblePlaybook):
-    def __init__(self, host, user: str, password: str) -> None:
+    def __init__(self, host, user: str, password: str, group: str = "admin") -> None:
         self.name = "common/createUser/createUser.yml"
-        self.params = {"host": host, "user": user, "password": password}
+        self.params = {"host": host, "user": user, "password": password, "group": group}
