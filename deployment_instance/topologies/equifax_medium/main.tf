27e78505d8c8af619c433232870f45af1a8ee74c
Brian Singer <brian@convolve.com>
2024-11-18 10:25:22 -0500

    More module cleanup

--- deployment_instance/topologies/equifax_medium/main.tf
+++ deployment_instance/topologies/equifax_medium/main.tf
@@ -1,17 +1,13 @@
 ######### Setup Networking #########
 module "perry_manager" {
-  source = "../modules/perry_manager"
+  source   = "../modules/perry_manager"
+  key_name = var.perry_key_name
 }
 
 module "attacker" {
   source             = "../modules/attacker"
   router_external_id = module.perry_manager.router_external_id
   key_name           = var.perry_key_name
-}
-
-resource "openstack_networking_network_v2" "manage_network" {
-  name           = "manage_network"
-  admin_state_up = "true"
 }
 
 resource "openstack_networking_network_v2" "webserver_network" {
@@ -27,14 +23,6 @@ resource "openstack_networking_network_v2" "critical_company_network" {
 }
 
 ### Subnets ###
-resource "openstack_networking_subnet_v2" "manage" {
-  name            = "manage"
-  network_id      = openstack_networking_network_v2.manage_network.id
-  cidr            = "192.168.198.0/24"
-  ip_version      = 4
-  dns_nameservers = ["8.8.8.8"]
-}
-
 resource "openstack_networking_subnet_v2" "webserver_subnet" {
   name            = "webserver_network"
   network_id      = openstack_networking_network_v2.webserver_network.id
@@ -52,55 +40,18 @@ resource "openstack_networking_subnet_v2" "critical_company_subnet" {
 }
 
 ### Routers ###
-resource "openstack_networking_router_v2" "router_external" {
-  name                = "router_external"
-  admin_state_up      = true
-  external_network_id = module.perry_manager.external_network_id
-}
-
-resource "openstack_networking_router_interface_v2" "router_interface_manage_external" {
-  router_id = openstack_networking_router_v2.router_external.id
-  subnet_id = openstack_networking_subnet_v2.manage.id
-}
-
 # Connect subnets
 resource "openstack_networking_router_interface_v2" "router_interface_manage_company" {
-  router_id = openstack_networking_router_v2.router_external.id
+  router_id = module.perry_manager.router_external_id
   subnet_id = openstack_networking_subnet_v2.webserver_subnet.id
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface_manage_datacenter" {
-  router_id = openstack_networking_router_v2.router_external.id
+  router_id = module.perry_manager.router_external_id
   subnet_id = openstack_networking_subnet_v2.critical_company_subnet.id
 }
 
 ######### Setup Compute #########
-### Management Host ###
-resource "openstack_compute_instance_v2" "manage_host" {
-  name        = "manage_host"
-  image_name  = "Ubuntu20"
-  flavor_name = "m1.small"
-  key_pair    = var.perry_key_name
-  security_groups = [
-    module.perry_manager.talk_to_manage_name,
-    module.perry_manager.manage_freedom_name
-  ]
-
-  network {
-    name = "manage_network"
-  }
-  depends_on = [openstack_networking_subnet_v2.manage]
-}
-
-resource "openstack_networking_floatingip_v2" "manage_floating_ip" {
-  pool = "external"
-}
-
-resource "openstack_compute_floatingip_associate_v2" "fip_manage" {
-  floating_ip = openstack_networking_floatingip_v2.manage_floating_ip.address
-  instance_id = openstack_compute_instance_v2.manage_host.id
-}
-
 ### Webserver Subnet Hosts ###
 resource "openstack_compute_instance_v2" "webserver" {
   count       = 2
@@ -123,25 +74,6 @@ resource "openstack_compute_instance_v2" "webserver" {
 }
 
 ### Corporate Subnet Hosts ###
-resource "openstack_compute_instance_v2" "employee" {
-  count       = 0
-  name        = "employee_${count.index}"
-  image_name  = "Ubuntu20"
-  flavor_name = "p2.tiny"
-  key_pair    = var.perry_key_name
-  security_groups = [
-    module.perry_manager.talk_to_manage_name,
-    openstack_networking_secgroup_v2.critical_company.name
-  ]
-
-  network {
-    name        = "critical_company_network"
-    fixed_ip_v4 = "192.168.201.${count.index + 10}"
-  }
-
-  depends_on = [openstack_networking_subnet_v2.critical_company_subnet]
-}
-
 resource "openstack_compute_instance_v2" "database" {
   count       = 24
   name        = "database_${count.index}"
--- deployment_instance/topologies/modules/perry_manager/perry_manager.tf
+++ deployment_instance/topologies/modules/perry_manager/perry_manager.tf
@@ -3,6 +3,10 @@ data "openstack_networking_network_v2" "external_network" {
   name = "external"
 }
 
+variable "key_name" {
+  description = "The name of the key pair to use for the instance"
+}
+
 output "external_network_id" {
   value = data.openstack_networking_network_v2.external_network.id
 }
@@ -26,123 +30,53 @@ output "router_external_id" {
   value = openstack_networking_router_v2.router_external.id
 }
 
-### Management rules, all servers need SSH access from management network ###
-resource "openstack_networking_secgroup_v2" "talk_to_manage" {
-  name                 = "talk_to_manage"
-  description          = ""
-  delete_default_rules = true
+### Network ###
+resource "openstack_networking_network_v2" "manage_network" {
+  name           = "manage_network"
+  admin_state_up = "true"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "manage_ssh_in" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "192.168.198.0/24"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "manage_ssh_out" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "192.168.198.0/24"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-# HTTPS
-resource "openstack_networking_secgroup_rule_v2" "manage_https_in" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "manage_https_out" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-# Attacker
-resource "openstack_networking_secgroup_rule_v2" "manage_attacker_in" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 8888
-  port_range_max    = 8888
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "manage_attacker_out" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 8888
-  port_range_max    = 8888
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-# Elasticsearch
-resource "openstack_networking_secgroup_rule_v2" "manage_elasticsearch_in" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 9200
-  port_range_max    = 9200
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-resource "openstack_networking_secgroup_rule_v2" "manage_elasticsearch_out" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 9200
-  port_range_max    = 9200
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.talk_to_manage.id
-}
-
-
-
-resource "openstack_networking_secgroup_v2" "manage_freedom" {
-  name        = "manage_freedom"
-  description = ""
-}
-
-resource "openstack_networking_secgroup_rule_v2" "manage_freedom_ssh_out" {
-  direction         = "egress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.manage_freedom.id
-}
-resource "openstack_networking_secgroup_rule_v2" "manage_freedom_ssh_in" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.manage_freedom.id
+resource "openstack_networking_subnet_v2" "manage" {
+  name            = "manage"
+  network_id      = openstack_networking_network_v2.manage_network.id
+  cidr            = "192.168.198.0/24"
+  ip_version      = 4
+  dns_nameservers = ["8.8.8.8"]
 }
 
 resource "openstack_networking_router_v2" "router_external" {
   name                = "router_external"
   admin_state_up      = true
   external_network_id = data.openstack_networking_network_v2.external_network.id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_manage_external" {
+  router_id = openstack_networking_router_v2.router_external.id
+  subnet_id = openstack_networking_subnet_v2.manage.id
+}
+
+### Host ###
+resource "openstack_compute_instance_v2" "manage_host" {
+  name        = "manage_host"
+  image_name  = "Ubuntu20"
+  flavor_name = "m1.small"
+  key_pair    = var.key_name
+  security_groups = [
+    openstack_networking_secgroup_v2.talk_to_manage.name,
+    openstack_networking_secgroup_v2.manage_freedom.name
+  ]
+
+  network {
+    name = "manage_network"
+  }
+  depends_on = [openstack_networking_subnet_v2.manage]
+}
+
+resource "openstack_networking_floatingip_v2" "manage_floating_ip" {
+  pool = "external"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_manage" {
+  floating_ip = openstack_networking_floatingip_v2.manage_floating_ip.address
+  instance_id = openstack_compute_instance_v2.manage_host.id
 }
