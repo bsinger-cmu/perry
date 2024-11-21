49b878c558bd854066ad85a4f55f4feeae9f1d22
Brian Singer <brian@convolve.com>
2024-11-21 15:51:23 -0500

    Fix ics and chain

--- environment/topologies/ics_inspired/main.tf
+++ environment/topologies/ics_inspired/main.tf
@@ -1,21 +1,14 @@
 ######### Setup Networking #########
 # External network is not managed by terraform, need to set as datasource
-data "openstack_networking_network_v2" "external_network" {
-  name = "external"
-}
-module "manage_rules" {
-  source = "../modules/"
+module "perry_manager" {
+  source   = "../modules/perry_manager"
+  key_name = var.perry_key_name
 }
 
-resource "openstack_networking_network_v2" "manage_network" {
-  name           = "manage_network"
-  admin_state_up = "true"
-}
-
-resource "openstack_networking_network_v2" "attacker_network" {
-  name           = "attacker_network"
-  admin_state_up = "true"
-  description    = "The attacker network"
+module "attacker" {
+  source             = "../modules/attacker"
+  router_external_id = module.perry_manager.router_external_id
+  key_name           = var.perry_key_name
 }
 
 resource "openstack_networking_network_v2" "employee_one_network" {
@@ -36,25 +29,7 @@ resource "openstack_networking_network_v2" "OT_network" {
   description    = "The corporate network with critical data"
 }
 
-
-
 ### Subnets ###
-resource "openstack_networking_subnet_v2" "manage" {
-  name            = "manage"
-  network_id      = openstack_networking_network_v2.manage_network.id
-  cidr            = "192.168.198.0/24"
-  ip_version      = 4
-  dns_nameservers = ["8.8.8.8"]
-}
-
-resource "openstack_networking_subnet_v2" "attacker_subnet" {
-  name            = "attacker_network"
-  network_id      = openstack_networking_network_v2.attacker_network.id
-  cidr            = "192.168.202.0/24"
-  ip_version      = 4
-  dns_nameservers = ["8.8.8.8"]
-}
-
 resource "openstack_networking_subnet_v2" "employee_one_subnet" {
   name            = "employee_one_subnet"
   network_id      = openstack_networking_network_v2.employee_one_network.id
@@ -79,69 +54,24 @@ resource "openstack_networking_subnet_v2" "OT_subnet" {
   dns_nameservers = ["8.8.8.8"]
 }
 
-
-
 ### Routers ###
-resource "openstack_networking_router_v2" "router_external" {
-  name                = "router_external"
-  admin_state_up      = true
-  external_network_id = data.openstack_networking_network_v2.external_network.id
-}
-
-resource "openstack_networking_router_interface_v2" "router_interface_manage_external" {
-  router_id = openstack_networking_router_v2.router_external.id
-  subnet_id = openstack_networking_subnet_v2.manage.id
-}
-
 # Connect subnets
 resource "openstack_networking_router_interface_v2" "router_interface_manage_company" {
-  router_id = openstack_networking_router_v2.router_external.id
+  router_id = module.perry_manager.router_external_id
   subnet_id = openstack_networking_subnet_v2.employee_one_subnet.id
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface_employee_two" {
-  router_id = openstack_networking_router_v2.router_external.id
+  router_id = module.perry_manager.router_external_id
   subnet_id = openstack_networking_subnet_v2.employee_two_subnet.id
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface_manage_datacenter" {
-  router_id = openstack_networking_router_v2.router_external.id
+  router_id = module.perry_manager.router_external_id
   subnet_id = openstack_networking_subnet_v2.OT_subnet.id
 }
 
-resource "openstack_networking_router_interface_v2" "router_interface_manage_attacker" {
-  router_id = openstack_networking_router_v2.router_external.id
-  subnet_id = openstack_networking_subnet_v2.attacker_subnet.id
-}
-
 ######### Setup Compute #########
-
-### Management Host ###
-resource "openstack_compute_instance_v2" "manage_host" {
-  name        = "manage_host"
-  image_name  = "Ubuntu20"
-  flavor_name = "m1.small"
-  key_pair    = var.perry_key_name
-  security_groups = [
-    module.manage_rules.talk_to_manage_name,
-    module.manage_rules.manage_freedom_name
-  ]
-
-  network {
-    name = "manage_network"
-  }
-  depends_on = [openstack_networking_subnet_v2.manage]
-}
-
-resource "openstack_networking_floatingip_v2" "manage_floating_ip" {
-  pool = "external"
-}
-
-resource "openstack_compute_floatingip_associate_v2" "fip_manage" {
-  floating_ip = openstack_networking_floatingip_v2.manage_floating_ip.address
-  instance_id = openstack_compute_instance_v2.manage_host.id
-}
-
 ### Employee 1 Subnet Hosts ###
 resource "openstack_compute_instance_v2" "employee_one" {
   count       = 10
@@ -150,7 +80,7 @@ resource "openstack_compute_instance_v2" "employee_one" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.employee_one_group.name
   ]
 
@@ -170,7 +100,7 @@ resource "openstack_compute_instance_v2" "manage_employee_one" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.employee_one_group.name
   ]
 
@@ -189,7 +119,7 @@ resource "openstack_compute_instance_v2" "employee_two" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.employee_two_group.name
   ]
 
@@ -208,7 +138,7 @@ resource "openstack_compute_instance_v2" "manage_employee_two" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.employee_two_group.name
   ]
 
@@ -226,7 +156,7 @@ resource "openstack_compute_instance_v2" "ot_sensors" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.ot_group.name
   ]
 
@@ -245,7 +175,7 @@ resource "openstack_compute_instance_v2" "ot_hosts" {
   flavor_name = "p2.tiny"
   key_pair    = var.perry_key_name
   security_groups = [
-    module.manage_rules.talk_to_manage_name,
+    module.perry_manager.talk_to_manage_name,
     openstack_networking_secgroup_v2.ot_group.name
   ]
 
@@ -255,23 +185,4 @@ resource "openstack_compute_instance_v2" "ot_hosts" {
   }
 
   depends_on = [openstack_networking_subnet_v2.OT_subnet]
-}
-
-
-### Attacker Subnet Hosts ###
-resource "openstack_compute_instance_v2" "attacker" {
-  name        = "attacker"
-  image_name  = "Ubuntu20"
-  flavor_name = "m1.small"
-  key_pair    = var.perry_key_name
-  security_groups = [
-    module.manage_rules.talk_to_manage_name,
-    openstack_networking_secgroup_v2.attacker.name
-  ]
-
-  network {
-    name = "attacker_network"
-  }
-
-  depends_on = [openstack_networking_subnet_v2.attacker_subnet]
 }
